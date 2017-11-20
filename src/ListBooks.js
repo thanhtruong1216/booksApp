918bcca6b45a6964ce1c743fa2150290ace2d12f
Thanh Thanh <thanhtruong1216@gmail.com>
2017-11-20 22:21:04 +0700

    Change class Listbooks to a function.

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import BookShelf from './BookShelf';
 import './ListBooks.css';
@@ -19,41 +19,38 @@ const BOOK_LISTS = [
   },
 ];
 
-class ListBooks extends Component {
-  static propTypes = {
-    onShelfChange: PropTypes.func.isRequired, 
-    books: PropTypes.arrayOf(
-      PropTypes.shape({
-        shelf: PropTypes.oneOf(['currentlyReading', 'wantToRead', 'read']).isRequired
-      })
-    ).isRequired
-  }
-
-  render() {
-    const { onShelfChange, books } = this.props;
-    return(
-      <div className="list-books">
-        <div>
-          { BOOK_LISTS.map(({title, shelfKey}) => {
-              const bookListBooks = books.filter((book) => book.shelf === shelfKey);
-              return (
-                <BookShelf
-                  key={shelfKey}
-                  numberOfBook={bookListBooks.length}
-                  bookshelfTitle={title}
-                  bookshelfBooks={bookListBooks}
-                  onShelfChange={onShelfChange}  
-                />
-              )
-            })
-          }
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
+function ListBooks(props) {
+  const { onShelfChange, books } = props;
+  return(
+    <div className="list-books">
+      <div>
+        { BOOK_LISTS.map(({title, shelfKey}) => {
+          const bookListBooks = books.filter((book) => book.shelf === shelfKey);
+          return (
+            <BookShelf
+              key={shelfKey}
+              numberOfBook={bookListBooks.length}
+              bookshelfTitle={title}
+              bookshelfBooks={bookListBooks}
+              onShelfChange={onShelfChange}  
+            />
+            )
+          })
+        }
+      </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
     </div>
-    );
-  }
+  );
 }
 
-export default ListBooks
+ListBooks.propTypes = {
+  onShelfChange: PropTypes.func.isRequired, 
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      shelf: PropTypes.oneOf(['currentlyReading', 'wantToRead', 'read']).isRequired
+    })
+  ).isRequired
+};
+export default ListBooks;
