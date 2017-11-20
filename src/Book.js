ef2139c9041778cc1601e89f555f5c89e530ad0d
Thanh Thanh <thanhtruong1216@gmail.com>
2017-11-20 21:19:54 +0700

    Add proptypes for book and onShelfChange. Use const instead of let.

--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './Book.css';
+import PropsTypes from 'prop-types';
 
 const DEFAULT_IMAGE_URL = 'https://www.123freevectors.com/wp-content/uploads/new/icon/102-red-book-icon-free-vector-illustration.png';
 
-class Book extends Component{
-  shelfChanger = (e) => {
+class Book extends Component {
+  static propsTypes = {
+    book: PropsTypes.string.isRequired,
+    onShelfChange: PropsTypes.func.isRequired
+  }
+  
+  shelfChanger = e => {
     const { value: shelf } = e.target;
     const { book, onShelfChange } = this.props;
     onShelfChange(book, shelf);
@@ -12,13 +18,12 @@ class Book extends Component{
 
   render() {
     const { book } = this.props;
-    let image = book.imageLinks ? book.imageLinks.thumbnail : DEFAULT_IMAGE_URL;
+    const image = book.imageLinks ? book.imageLinks.thumbnail : DEFAULT_IMAGE_URL;
     
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{backgroundImage: `url("${ image}")`}}
-          />
+          <div className="book-cover" style={{backgroundImage: `url("${ image}")`}}/>
           <div className="book-shelf-changer">
             <select onChange={this.shelfChanger} value={book.shelf}>
               <option value="none" disabled>Move to...</option>
@@ -35,5 +40,4 @@ class Book extends Component{
     );
   }   
 }
-
-export default Book
+export default Book;
